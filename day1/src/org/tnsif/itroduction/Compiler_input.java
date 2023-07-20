8166227cf32c34a541b10d4c9085381b93cf38dd
Abhishek Mahale <96944806+1472abhi@users.noreply.github.com>
2023-07-20 14:07:14 +0530

    first comit

---
+++ day1/src/org/tnsif/itroduction/Compiler_input.java
@@ -0,0 +1,13 @@
+//program to demonstrate on compile-time-input
+package org.tnsif.itroduction;
+
+public class Compiler_input {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		short a=10;
+		System.out.println("the number is"+a);
+
+	}
+
+}
